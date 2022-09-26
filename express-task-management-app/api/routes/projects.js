5ffc3d7d51d558bfc21c5b124209967ec42455be
Jerome Pineda <jeromepineda79@gmail.com>
2022-09-26 14:24:59 -0700

    addded get and post functions to projects.js

--- express-task-management-app/api/routes/projects.js
+++ express-task-management-app/api/routes/projects.js
@@ -1,1 +1,36 @@
-//Blank
+const router = require("express").Router();
+
+// require in our mock data to support our API responses
+const projects = require("../../../mock-data/projects.json");
+
+// GET /projects
+router.get("/", (req, res) =>{
+    // query params use the ?key=value
+    // my API is oging to use a key called name
+
+    const {query} = req;
+    const name = query.name;
+
+    let all = projects;
+
+    // if query param name was used
+    // filter by name of the quiz matching name passed in by user
+
+    if (name) {
+        all = projects.filter((project) =>{
+            return project.name.toLocaleLowerCase().includes(name.toLocaleLowerCase());
+        })
+    }
+
+    res.json(all);
+})
+// Post/ projects
+router.post("/", (req, res) =>{
+    const {body} = req;
+
+    const id = projects.length + 1;
+    res.json({...body, id});
+})
+
+
+module.exports = router;
--- express-task-management-app/server.js
+++ express-task-management-app/server.js
@@ -2,14 +2,15 @@
 
 // require in our dependencies
 const cors = require("cors");
-const exprees = require("express");
+const express = require("express");
 
 // call the express functions which create the express applicaiton
 // this allows us to use the full functionality of our express application
 const app = express();
-const port = 888;
+const port = 8888;
 
-// TODO: require in our route resource
+// require in our route resource
+const projects = require("./api/routes/projects")
 
 // apply our cors middleware
 app.use(cors());
@@ -17,7 +18,8 @@ app.use(cors());
 // middleware to pares POST/PUT bodies express
 app.use(express.json());
 
-// TODO: add resouce route to our express app
+// add resouce route to our express app
+app.use("/projects", projects)
 
 //start the server
 app.listen(port, () =>{
